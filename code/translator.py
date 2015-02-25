af0b4d69cb62612cd48aa87ee5484e0369a8a46d
Zach Ellison <zellison@stanford.edu>
2015-02-24 23:37:24 -0800

    Adding POS tagging. Wrote one post-processing method that swaps nouns and adjectives, but saw no performance increase because it doesn't occur in dev set. I also edited the dictionary to de-capitalize translation to 'Sore', so it's now 'sore' because all other translations are in lower case.

--- code/translator.py
+++ code/translator.py
@@ -7,12 +7,18 @@ import operator
 import sets
 import re
 import string
+import nltk
 
+# ======== GLOBALS ==========
 devFile = '../corpus/dev.txt'
 testFile = '../corpus/test.txt'
 dictFile = '../dic/dict.txt'
 
+# Variable gives the n-gram used in computation of the BLEU score for
+# each translation
 nGramForBLEU = 3
+
+# ==========================
 
 
 # Parses the training file given in dev.txt
@@ -69,13 +75,73 @@ def directTranslate(spanishSentences, dictionary):
         translations.append(translatedSentence)
     return translations
 
-def printTranslations(spanishSentences, englishTranslations, englishSentences, bScores):
+# ============= Pre-Processing Methods ============== #
+
+
+
+# =================================================== #
+
+# ============ Post-Processing Methods ============== #
+
+# Post-Process #1:
+
+# Method looks for any nouns followed by adjectives in the translation and swaps them
+# unless it's following a verb because I noticed that it is the one case where we
+# actually don't want to swap them
+
+# No occurences in our dev set of a noun followed by an adjective and not preceded by a verb,
+# so it didn't end up improving performance on our dev set
+
+def nounAdjectiveSwap(taggedEnglishTranslations):
+    updatedTranslations = []
+    for sentence in taggedEnglishTranslations:
+        for idx in range(1, len(sentence)):
+            # Check if Adjective (JJ) is following a noun (NN)
+            if 'NN' in sentence[idx-1][1] and 'JJ' in sentence[idx][1]:
+                # If it's following a verb, don't swap
+                if sentence[idx-2] and 'VB' in sentence[idx-2][1]: continue
+                # Otherwise, swap the two words
+                swapWord = sentence[idx-1]
+                sentence[idx-1] = sentence[idx]
+                sentence[idx] = swapWord
+        updatedTranslations.append(sentence)
+    return updatedTranslations
+
+
+# =================================================== #
+
+# Method takes a list of English Translation sentences and returns a POS tagged sentence
+
+def posTagTranslations(englishTranslations):
+    pos_tagged_translations = []
+    for sentence in englishTranslations:
+        pos_tagged_translations.append(nltk.pos_tag(sentence))
+#        print "Tagged Sentence: ", pos_tagged_translations[-1]
+    return pos_tagged_translations
+
+# Method takes a list of POS tagged sentences and returns a list of normal translated
+# sentences
+
+def unPosTagTranslations(taggedEnglishTranslations):
+    englishTranslations = []
+    for tagged_sentence in taggedEnglishTranslations:
+        sentence = []
+        for word, tag in tagged_sentence:
+            sentence.append(word)
+        englishTranslations.append(sentence)
+    return englishTranslations
+
+# Method walks through each transation and prints the Spanish sentence, the machine translation
+# and the correct translation as well as the BLEU score of the translation.
+
+def printTranslations(spanishSentences, englishTranslations, englishSentences, bScores, directTranslationBScores):
     for idx, sentence in enumerate(englishTranslations):
         print "============= New Translation ============="
         print "Translation of: ", " ".join(spanishSentences[idx])
         print "Translation: ", " ".join(sentence)
         print "Correct Translation: ", " ".join(englishSentences[idx])
-        print "BLEU-", nGramForBLEU," Score: ", bScores[idx]
+        print "Direct Translation BLEU-", nGramForBLEU," Score: ", directTranslationBScores[idx]
+        print "Final BLEU-", nGramForBLEU," Score: ", bScores[idx]
 
 # Method computes the BLEU score for all Translations given in the list englishTranslations
 # when compared against the correct translation given by englishSentences. Uses n-grams given
@@ -96,7 +162,7 @@ def computeBLEU(englishTranslations, englishSentences, n):
             for pos in range(n_gram_count):
                 substr = ' '.join(translation[pos: pos+i])
                 if substr in matches:
-                    matches[substr] += 1.0
+                    matches[substr] += 1
                     if ' '.join(correctTranslation).count(substr) >= matches[substr]:
                         ref_count += 1.0
                 elif substr in ' '.join(correctTranslation):
@@ -109,9 +175,23 @@ def computeBLEU(englishTranslations, englishSentences, n):
 def main():
     spanishSentences, englishSentences = parseTrainFile(devFile)
     dictionary = parseDict(dictFile)
+
+    # Pre-processing methods
+    
+    # Direct Translation
     englishTranslations = directTranslate(spanishSentences, dictionary)
+    directTranslationBScores = computeBLEU(englishTranslations, englishSentences, nGramForBLEU)
+
+    # Post-processing methods
+    taggedEnglishTranslations = posTagTranslations(englishTranslations)
+
+    taggedEnglishTranslations = nounAdjectiveSwap(taggedEnglishTranslations)
+    
+    englishTranslations = unPosTagTranslations(taggedEnglishTranslations)
+
+    # Scoring
     bScores = computeBLEU(englishTranslations, englishSentences, nGramForBLEU)
-    printTranslations(spanishSentences, englishTranslations, englishSentences, bScores)
+    printTranslations(spanishSentences, englishTranslations, englishSentences, bScores, directTranslationBScores)
 
 # Run
 main()
