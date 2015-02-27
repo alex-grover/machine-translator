5ea10d9228248c22b50f9fc4cdd8389e6eb2fdcc
Alex Grover <ajgrover@stanford.edu>
2015-02-26 19:16:11 -0800

    Use files from args and refactor for readability.

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # machine-translator
 Final project for Stanford CS 124. Translates Spanish sentences to English using Machine Translation techniques.
 
+Required setup:
+```bash
+sudo pip install ntlk
+python
+>>> import nltk
+>>> nltk.download()
+```
+
+And download all from the website.
+
 To run on the dev set:
 ```bash
-python code/translator.py corpus/dev.txt
+python code/translator.py dic/dict.txt corpus/dev.txt
 ```
 
 To run on the test set:
 ```bash
-python code/translator.py corpus/test.txt
+python code/translator.py dic/dict.txt corpus/test.txt
 ```
--- code/translator.py
+++ code/translator.py
@@ -13,41 +13,51 @@ import nltk
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-# ======== GLOBALS ==========
-devFile = '../corpus/dev.txt'
-testFile = '../corpus/test.txt'
-dictFile = '../dic/dict.txt'
-
 # Variable gives the n-gram used in computation of the BLEU score for
-# each translation
+# each translation.
 nGramForBLEU = 3
 
-# ==========================
+
+# Parses the dictionary passed in.
+# Format is 'EnglishWord:SpanishWord', all lowercase
+# Returns a dict of 'Spanish Word' : 'English Word'
+def parseDict(filename):
+    dictionary = {}
+    f = open(filename)
+    for line in f:
+        line = line.strip('\n')
+        spanishWord, englishWord = line.split(':')
+        dictionary[spanishWord] = englishWord
+    return dictionary
 
 
-# Parses the training file given in dev.txt
-# Returns a list of sentences, where each sentence is given
-# as a list of the words in that sentence
-
-# Strips all punctuation (both English and Spanish) and converts
-# to lower case
-
+# Parses the training file passed in. Returns a list of sentences,
+# where each sentence is given as a list of the words in that sentence.
+# Strips all punctuation (both English and Spanish) and converts to
+# lower case.
 def parseTrainFile(filename):
     spanishSentences = []
     englishSentences = []
     rawEnglishSentences = []
     rawSpanishSentences = []
     prevLine = 'English'
+
     f = open(filename)
     for line in f:
-        if line[0] is '#': continue
+        # Ignore comments
+        if line[0] is '#':
+            continue
+
+        # Tokenize word using NLTK and ignore empty lines
+        sentence = nltk.word_tokenize(line)
+        if not sentence:
+            continue
+
         raw_line = line
 
-# Pre-processing strategy #1: Throw out Spanish punctuation
+        # Pre-processing strategy #1: Throw out Spanish punctuation
         line = line.strip('¡¿')
 
-        sentence = nltk.word_tokenize(line)
-        if not sentence: continue
         if prevLine is 'English':
             rawSpanishSentences.append(raw_line)
             spanishSentences.append(sentence)
@@ -58,34 +68,62 @@ def parseTrainFile(filename):
             prevLine = 'English'
     return (spanishSentences, englishSentences, rawEnglishSentences, rawSpanishSentences)
 
-# Parses the dictionary given in dict.txt
-# Format is 'EnglishWord:SpanishWord', all lowercase
-# Returns a dict of 'Spanish Word' : 'English Word'
-
-def parseDict(filename):
-    dictionary = {}
-    f = open(filename)
-    for line in f:
-        line = line.strip('\n')
-        spanishWord, englishWord = line.split(':')
-        dictionary[spanishWord] = englishWord
-    return dictionary
 
 # Returns a list of lists with a direct translation from Spanish to English
 # i.e. returns a sentence for which each Spanish word is replaced with its
 # English translation
-
 def directTranslate(spanishSentences, dictionary):
     translations = []
+
     for sentence in spanishSentences:
         translatedSentence = []
+
         for word in sentence:
-            if word in dictionary: translatedSentence.append(dictionary[word])
-            elif word.lower() in dictionary: translatedSentence.append(dictionary[word.lower()])
-            # Catches Numbers for now, might need to change this to check that it's numeric first
-            else: translatedSentence.append(word)
+            word = word.lower()
+            if word in dictionary:
+                translatedSentence.append(dictionary[word])
+            else:
+                translatedSentence.append(word)
+
         translations.append(translatedSentence)
+
     return translations
+
+
+# Method computes the BLEU score for all Translations given
+# in the list englishTranslations when compared against the
+# correct translation given by englishSentences. Returns a list
+# of BLEU scores corresponding to the translations given in\
+# englishTranslations.
+def computeBLEU(englishTranslations, englishSentences):
+    bScores = []
+    for i, sentence in enumerate(englishTranslations):
+        score = min(1.0, float(len(sentence)) / float(len(englishSentences[i])))
+
+        for j in range(1, nGramForBLEU + 1):
+            translation = englishTranslations[i]
+            correctTranslation = englishSentences[i]
+            n_gram_count = len(translation) + 1 - j
+            ref_count = 0.0
+            matches = {}
+
+            for pos in range(n_gram_count):
+                substr = ' '.join(translation[pos: pos+j])
+
+                if substr in matches:
+                    matches[substr] += 1
+                    if ' '.join(correctTranslation).count(substr) >= matches[substr]:
+                        ref_count += 1.0
+                elif substr in ' '.join(correctTranslation):
+                    ref_count += 1.0
+                    matches[substr] = 1
+
+            score *= ref_count / n_gram_count
+
+        bScores.append(score)
+
+    return bScores
+
 
 # ============= Pre-Processing Methods ============== #
 
@@ -96,75 +134,88 @@ def directTranslate(spanishSentences, dictionary):
 
 # Post-Process #1:
 
-# Method looks for any nouns followed by adjectives in the translation and swaps them
-# unless it's following a verb because I noticed that it is the one case where we
-# actually don't want to swap them
-
-# No occurences in our dev set of a noun followed by an adjective and not preceded by a verb,
-# so it didn't end up improving performance on our dev set
-
+# Method looks for any nouns followed by adjectives in the translation
+# and swaps them unless it's following a verb because we noticed that it
+# is the one case where we actually don't want to swap them. No
+# occurences in our dev set of a noun followed by an adjective and not
+# preceded by a verb, so it didn't end up improving performance on our
+# dev set. May help improve accuracy for other cases.
 def nounAdjectiveSwap(taggedEnglishTranslations):
     updatedTranslations = []
+
     for sentence in taggedEnglishTranslations:
         prev = False
-        for idx in range(1, len(sentence)):
-            # Check if previous word was swapped, i.e. the word I'm looking at was just
-            # swapped into this position
+
+        for i in xrange(1, len(sentence)):
+            # Check if previous word was swapped, i.e. the word
+            # I'm looking at was just swapped into this position
             if prev:
                 prev = False
                 continue
+
             # Check if Adjective (JJ) is following a noun (NN)
-            if 'NN' in sentence[idx-1][1] and 'JJ' in sentence[idx][1]:
+            if 'NN' in sentence[i - 1][1] and 'JJ' in sentence[i][1]:
                 # If it's following a verb, don't swap
-                if sentence[idx-2] and 'VB' in sentence[idx-2][1]: continue
+                if sentence[i - 2] and 'VB' in sentence[i - 2][1]:
+                    continue
+
                 # Otherwise, swap the two words
-                swapWord = sentence[idx-1]
-                sentence[idx-1] = sentence[idx]
-                sentence[idx] = swapWord
+                swapWord = sentence[i - 1]
+                sentence[i - 1] = sentence[i]
+                sentence[i] = swapWord
                 prev = True
+
         updatedTranslations.append(sentence)
+
     return updatedTranslations
+
 
 # Post-Process #2:
 
 # Spanish uses the negation (e.g. the word 'no') before the verb whereas
 # it is used following the verb in English (e.g. 'is not')
-
 def verbNegation(taggedEnglishTranslations):
     updatedTranslations = []
+
     for sentence in taggedEnglishTranslations:
         prev = False
-        for idx in range(1, len(sentence)):
-            # Check if previous word was swapped, i.e. the word I'm looking at was just
-            # swapped into this position
+
+        for i in range(1, len(sentence)):
+            # Check if previous word was swapped, i.e. the word
+            # I'm looking at was just swapped into this position
             if prev:
                 prev = False
                 continue
-            # Check if 'no' is following a verb
-            if 'no' == sentence[idx-1][0] and ('VB' in sentence[idx][1] or 'MD' in sentence[idx][1]):
-                swap = sentence[idx - 1]
-                sentence[idx - 1] = sentence[idx]
-                sentence[idx] = ('not', swap[1])
-                prev = True
-        updatedTranslations.append(sentence)
-    return updatedTranslations
 
+            # Check if 'no' is following a verb
+            if 'no' == sentence[i - 1][0] and \
+                ('VB' in sentence[i][1] or 'MD' in sentence[i][1]):
+                    swap = sentence[i - 1]
+                    sentence[i - 1] = sentence[i]
+                    sentence[i] = ('not', swap[1])
+                    prev = True
+
+        updatedTranslations.append(sentence)
+
+    return updatedTranslations
 
 
 # =================================================== #
 
-# Method takes a list of English Translation sentences and returns a POS tagged sentence
 
+# Method takes a list of English Translation sentences and
+# returns a POS tagged sentence.
 def posTagTranslations(englishTranslations):
     pos_tagged_translations = []
+
     for sentence in englishTranslations:
         pos_tagged_translations.append(nltk.pos_tag(sentence))
-#        print "Tagged Sentence: ", pos_tagged_translations[-1]
+
     return pos_tagged_translations
 
-# Method takes a list of POS tagged sentences and returns a list of normal translated
-# sentences
 
+# Method takes a list of POS tagged sentences and returns a
+# list of normal translated sentences
 def unPosTagTranslations(taggedEnglishTranslations):
     englishTranslations = []
     for tagged_sentence in taggedEnglishTranslations:
@@ -174,76 +225,59 @@ def unPosTagTranslations(taggedEnglishTranslations):
         englishTranslations.append(sentence)
     return englishTranslations
 
-# Method walks through each transation and prints the Spanish sentence, the machine translation
-# and the correct translation as well as the BLEU score of the translation.
 
+# Method walks through each transation and prints the Spanish
+# sentence, the machine translation and the correct translation
+# as well as the BLEU score of the translation.
 def printTranslations(spanishSentences, englishTranslations, englishSentences, rawEnglishTranslations, rawSpanishSentences, bScores, directTranslationBScores, taggedEnglishTranslations):
     print "\n\n"
     print "================ TRANSLATIONS =================="
     print "\n\n"
-    for idx, sentence in enumerate(englishTranslations):
+    for i, sentence in enumerate(englishTranslations):
         print
-        print "============= New Translation: {0} =============".format(idx + 1)
-        print "Translation of: ", " ".join(spanishSentences[idx])
+        print "============= New Translation: {0} =============".format(i + 1)
+        print "Translation of: ", " ".join(spanishSentences[i])
         print "Translation: ", " ".join(sentence)
-        print "Tagged Translation: ", taggedEnglishTranslations[idx]
-        print "Correct Translation: ", " ".join(englishSentences[idx])
-        print "Raw Spanish Sentence: ", rawSpanishSentences[idx]
-        print "Raw Correct Translation: ", rawEnglishTranslations[idx]
-        print "Direct Translation BLEU-", nGramForBLEU," Score: ", directTranslationBScores[idx]
-        print "Final BLEU-", nGramForBLEU," Score: ", bScores[idx]
+        print "Tagged Translation: ", taggedEnglishTranslations[i]
+        print "Correct Translation: ", " ".join(englishSentences[i])
+        print "Raw Spanish Sentence: ", rawSpanishSentences[i]
+        print "Raw Correct Translation: ", rawEnglishTranslations[i]
+        print "Direct Translation BLEU-", nGramForBLEU," Score: ", directTranslationBScores[i]
+        print "Final BLEU-", nGramForBLEU," Score: ", bScores[i]
 
-# Method computes the BLEU score for all Translations given in the list englishTranslations
-# when compared against the correct translation given by englishSentences. Uses n-grams given
-# by the input variable n.
-
-# Returns a list of BLEU scores corresponding to the translations given in englishTranslations
-
-def computeBLEU(englishTranslations, englishSentences, n):
-    bScores = []
-    for idx, sentence in enumerate(englishTranslations):
-        score = min(1.0, float(len(sentence)) / float(len(englishSentences[idx])))
-        for i in range(1, n+1):
-            translation = englishTranslations[idx]
-            correctTranslation = englishSentences[idx]
-            n_gram_count = len(translation) + 1 - i
-            ref_count = 0.0
-            matches = {}
-            for pos in range(n_gram_count):
-                substr = ' '.join(translation[pos: pos+i])
-                if substr in matches:
-                    matches[substr] += 1
-                    if ' '.join(correctTranslation).count(substr) >= matches[substr]:
-                        ref_count += 1.0
-                elif substr in ' '.join(correctTranslation):
-                    ref_count += 1.0
-                    matches[substr] = 1
-            score *= ref_count / n_gram_count
-        bScores.append(score)
-    return bScores
 
 def main():
-    spanishSentences, englishSentences, rawEnglishSentences, rawSpanishSentences = parseTrainFile(devFile)
+    if len(sys.argv) != 3:
+        print "Usage: python translator.py [dictFile] [translateFile]"
+        sys.exit(1)
+
+    dictFile = sys.argv[1]
+    translateFile = sys.argv[2]
+
     dictionary = parseDict(dictFile)
+    spanishSentences, englishSentences, rawEnglishSentences, rawSpanishSentences = parseTrainFile(translateFile)
 
     # Pre-processing methods
-    
+
+
     # Direct Translation
     englishTranslations = directTranslate(spanishSentences, dictionary)
-    directTranslationBScores = computeBLEU(englishTranslations, englishSentences, nGramForBLEU)
+    directTranslationBScores = computeBLEU(englishTranslations, englishSentences)
+
 
     # Post-processing methods
     taggedEnglishTranslations = posTagTranslations(englishTranslations)
 
     taggedEnglishTranslations = nounAdjectiveSwap(taggedEnglishTranslations)
     taggedEnglishTranslations = verbNegation(taggedEnglishTranslations)
-    
+
     englishTranslations = unPosTagTranslations(taggedEnglishTranslations)
 
+
     # Scoring
-    bScores = computeBLEU(englishTranslations, englishSentences, nGramForBLEU)
+    bScores = computeBLEU(englishTranslations, englishSentences)
     printTranslations(spanishSentences, englishTranslations, englishSentences, rawEnglishSentences, rawSpanishSentences, bScores, directTranslationBScores, taggedEnglishTranslations)
 
-# Run
-main()
 
+if __name__ == '__main__':
+    main()
